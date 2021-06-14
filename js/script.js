b7cbfda5e63abbcebea2c502b13e76d1b06dde6c
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-06-14 14:20:50 +0200

    oooook

--- js/script.js
+++ js/script.js
@@ -5,9 +5,29 @@ Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
 // faccio un array con 5 numeri random
+document.addEventListener('DOMContentLoaded', function () {
+
+var difficoltà = parseInt( prompt('Scegli difficoltà tra 0, 1 o 2.') );
+
+
+switch(difficoltà) {
+    case 0:
+      numToRemember = 5;
+      break;
+    case 1:
+      numToRemember = 10;
+      break;
+    case 2:
+      numToRemember = 15;
+      break;
+    default:
+        alert('LIvello di difficoltà non accettato')
+    };
+    
+
 var randomNumber = [];
 
-while(randomNumber.length<5){
+while(randomNumber.length<numToRemember){
     var valore = Math.floor(Math.random()*100 +1);
     if (randomNumber.includes(valore) == false){//if(!randomNumber.includes(valore)) posso scrivere anche cosi e la stessa cosa
         randomNumber.push(valore);
@@ -21,11 +41,11 @@ var userNumbers = [];
 var winNumbers = []
 
 setTimeout(function() {
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < numToRemember; i++) {
     var numbers = parseInt( prompt('Inserisci uno dei cinque numeri memorizzati') );
     
-    while ( isNaN(numbers) || numbers < 1 || numbers > 100 ) {
-      numbers = parseInt( prompt('Prego, inserisci un numero valido da 1 a 100') );
+    while ( isNaN(numbers) || numbers < 1 || numbers > 100 ||  (userNumbers.includes(numbers)) ) {
+      numbers = parseInt( prompt('Prego, inserisci un numero valido da 1 a 100 e solo una volta') );
     };
 
     if ( !userNumbers.includes(numbers) ) {
@@ -51,7 +71,5 @@ setTimeout(function() {
   
 }, 30000);
 
-// Funzioni
-function randomNumbers(max) {
-  return Math.floor( Math.random() * max ) + 1;
-};
+
+});
